5f70082a758555c0df5df254b7d8027b6a45e337
CodeEagle <stasura@hotmail.com>
2025-03-16 23:15:09 +0800

    Update diy-part1.sh
    add homeproxy

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy' >>feeds.conf.default
